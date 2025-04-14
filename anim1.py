86b89cadacfbdc886a41a46e0f4a70cce3b6ddb9
tomciolowsky <tomciolowsky@gmail.com>
2025-04-14 20:40:52 +0200

    Róźne rozmiary i długości kreatur. Kropki jako obrys sylwetki

--- anim1.py
+++ anim1.py
@@ -1,29 +1,48 @@
 import pygame
 from configuration import *
+from math import sin, cos
 
 class Head_node(pygame.sprite.Sprite):
-    def __init__(self, pos, display_surf, groups):
+    def __init__(self, pos, display_surf, size, groups):
         super().__init__(groups)
         self.spawn_pos = pos
         self.direction = pygame.Vector2()
         self.speed = 150
-        self.size = (NODE_PARAMS['DIAMETER'], NODE_PARAMS['DIAMETER'])
-
+        self.size = (size, size)
+        # trying with different circle diameters AND different rect sizes
         self.image = pygame.Surface(self.size)
         self.image.set_colorkey("black")
-        pygame.draw.circle(self.image, "white", NODE_PARAMS['RADIUSXRADIUS'], NODE_PARAMS['DIAMETER']/2, NODE_PARAMS['WIDTH'])
-        pygame.draw.circle(self.image, "red", NODE_PARAMS['RADIUSXRADIUS'], 4, 4)
+        pygame.draw.circle(self.image, "white", (size/2, size/2), size/2, NODE_PARAMS['WIDTH'])
+        pygame.draw.circle(self.image, "red", (size/2, size/2), 4, 4)
 
         self.rect = self.image.get_frect(center=self.spawn_pos)
+
+        #
+        self.dot1_surf = pygame.Surface(self.size)
+        self.dot1_surf.set_colorkey("black")
+        pygame.draw.circle(self.dot1_surf, "red", (size/2, size/2), 4, 4)
+        self.dot1_rect = self.dot1_surf.get_frect(center=self.spawn_pos)
+        #
+
 
         self.display_surf = display_surf
 
     def draw(self):
         self.display_surf.blit(self.image, self.rect)
+        self.display_surf.blit(self.dot1_surf, self.dot1_rect)
+
+    def draw_dots(self):
+        
+        rotated0 = (self.rect.centerx + (self.direction[0])*self.size[0]/2)
+        rotated1 = (self.rect.centery + (self.direction[1])*self.size[0]/2)
+        center0 = (rotated0,rotated1)
+        self.dot1_rect.center = center0
 
     def update(self, dt):
-        mouse_pos = pygame.mouse.get_pos()
         dont_move = False
+        mouse_pos = pygame.mouse.get_pos()
+        if pygame.mouse.get_pressed()[0]:
+            dont_move = False
         if abs(mouse_pos[0] - self.rect.centerx) < 0.1 and abs(mouse_pos[1] - self.rect.centery) < 0.1:
             dont_move = True
         else:
@@ -33,30 +52,32 @@ class Head_node(pygame.sprite.Sprite):
 
         if not dont_move:
             self.rect.center += self.direction * self.speed * dt
+        self.draw_dots()
         self.draw()
 
 
 class Body_node(Head_node):
-    def __init__(self, pos, display_surf, groups):
-        super().__init__(pos, display_surf, groups)
+    def __init__(self, pos, display_surf, size, groups):
+        super().__init__(pos, display_surf, size, groups)
+        self.direction = pygame.Vector2()
+        self.size = (size, size)
+        self.image = pygame.Surface(self.size)
+        self.image.set_colorkey("black")
+        pygame.draw.circle(self.image, "white", (size/2, size/2), size/2, NODE_PARAMS['WIDTH'])
+        pygame.draw.circle(self.image, "red", (size/2, size/2), 2, 2)
 
-        # self.head_pos = head_pos
-        self.distance_vector = pygame.Vector2()
+        self.rect = self.image.get_frect(center=self.spawn_pos)
 
-        # self.distance_vector[0] = self.rect.centerx - head_pos[0]
-        # self.distance_vector[1] = self.rect.centery - head_pos[1]
-        # self.distance_vector = self.distance_vector.normalize() * 32
-
-        # self.rect.centerx = self.distance_vector[0] + head_pos[0]
-        # self.rect.centery = self.distance_vector[1] + head_pos[1]
 
     def get_head_pos(self, head_pos):
         self.head_pos = head_pos
 
     def update(self, dt):
-        self.distance_vector[0] = self.rect.centerx - self.head_pos[0]
-        self.distance_vector[1] = self.rect.centery - self.head_pos[1]
-        self.distance_vector = self.distance_vector.normalize() * NODE_PARAMS['DIAMETER']/2 if self.distance_vector else self.distance_vector
+        self.direction[0] = self.rect.centerx - self.head_pos[0]
+        self.direction[1] = self.rect.centery - self.head_pos[1]
+        self.direction = self.direction.normalize() if self.direction else self.direction
+
+        self.distance_vector = self.direction * 32 # The nodes are spread evenly
 
         self.rect.centerx = self.distance_vector[0] + self.head_pos[0]
         self.rect.centery = self.distance_vector[1] + self.head_pos[1]
@@ -72,11 +93,24 @@ running = True
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+body_nodes_sprites = pygame.sprite.Group()
 
-node1 = Head_node((500, 500), display_surface, all_sprites)
-node2 = Body_node((0,0), display_surface, all_sprites)
-node3 = Body_node((0, 0), display_surface, all_sprites)
-node4 = Body_node((0, 0), display_surface, all_sprites)
+
+def create_creature(creature_length, creature_body_array):
+    # max_size_of_node = max(creature_body_array)
+    Head_node((500, 500), display_surface, creature_body_array[0], all_sprites)
+    for i in range(1, creature_length):
+        Body_node((0, 0), display_surface, creature_body_array[i], (all_sprites, body_nodes_sprites))
+
+# TODO random creation of interesting creatures:
+# import numpy as np
+# samples = np.random.lognormal(mean=3.5, sigma=0.4, size=14).astype(int)
+# creature_body_array2 = (sorted(samples, reverse=True))
+
+creature_body_array = [52, 58, 40, 60, 68, 71, 65, 50, 28, 15, 21, 19, 13, 13]
+create_creature(len(creature_body_array), creature_body_array)
+
+dots_coords = [] # TODO make a list of the 'dots-coordinates' and connect them with lines
 
 while running:
 
@@ -88,12 +122,22 @@ while running:
     
     display_surface.fill("darkblue")
 
-    node2.get_head_pos(node1.rect.center)
-    node3.get_head_pos(node2.rect.center)
-    node4.get_head_pos(node3.rect.center)
+
+    for node, its_head in zip(body_nodes_sprites, all_sprites):
+        node.get_head_pos(its_head.rect.center)
+
+
+    for node in all_sprites:
+        for sign in [1, -1]:
+            rotated_V = (-sign*node.direction[1], sign*node.direction[0])
+            rotated0 = (node.rect.centerx + (rotated_V[0])*node.size[0]/2)
+            rotated1 = (node.rect.centery + (rotated_V[1])*node.size[0]/2)
+            pygame.draw.circle(display_surface, "red", (rotated0, rotated1), 4, 4)
+        
+
     all_sprites.update(dt)
 
-    all_sprites.draw(display_surface)
+    body_nodes_sprites.draw(display_surface)
 
     pygame.display.update()
 
